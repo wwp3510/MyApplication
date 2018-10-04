d26b7874f6ba6a732fe129ff5f03422f22e15446
cameronriu <cameronriu@mjrtech.com>
2018-10-04 14:25:10 -0400

    tutorial commit

---
+++ app/src/main/java/com/example/wwp3510/myapplication/activity_cameronriu.java
@@ -0,0 +1,13 @@
+package com.example.wwp3510.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_cameronriu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cameronriu);
+    }
+}
